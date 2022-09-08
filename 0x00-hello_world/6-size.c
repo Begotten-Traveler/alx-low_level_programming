c020850b22aaf4232d886dcfc8bd424e1522d71b
solomondev01 <solomondev002@gmail.com>
2022-09-08 08:48:13 -0700

    Betty coding style and size32, size64

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
