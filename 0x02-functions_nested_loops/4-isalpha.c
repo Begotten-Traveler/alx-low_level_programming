94f8383b50854d10bf65bd728aef5e2fd14b7354
solomondev01 <solomondev002@gmail.com>
2022-09-13 05:32:59 -0700

    expression error fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,9 +5,9 @@
  * @c: The character in ASCII code
  * Return: 1 for letters. 0 for the rest of it.
  */
-int int _isalpha(int c)
+int _isalpha(int c)
 {
-	if ((c >= 97 && <= 122) || (c >= 65 && c <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
