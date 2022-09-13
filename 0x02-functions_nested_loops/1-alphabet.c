d8de26f4b94c6c786056f30aabe92b20428ffd5d
solomondev01 <solomondev002@gmail.com>
2022-09-13 04:48:29 -0700

    betty coding style block comment resolved

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/**print_alphabet - Prints the alphabet in lowercase.
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
