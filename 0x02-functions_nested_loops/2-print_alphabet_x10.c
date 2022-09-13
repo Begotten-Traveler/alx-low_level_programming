3b58f9973c741f7b802d7b1fff297b01502d2f65
solomondev01 <solomondev002@gmail.com>
2022-09-13 05:01:46 -0700

    betty description resolved

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10.c - Prints the alphabet 10 times.
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
