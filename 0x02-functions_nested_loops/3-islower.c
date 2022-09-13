2091ff0a6bc3088af23fff7f2f33c324009b3a3e
solomondev01 <solomondev002@gmail.com>
2022-09-13 05:21:19 -0700

    betty error fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * _islower - Shows 1 if the input is a lowercase character, in other cases shows 0
- * @c - An ASCII code
+ * _islower - Shows 1 if the input is a lowercase character,
+ * in other cases shows 0
+ * @c: The character in ASCII code
  * Return: 1 for lowercase chracter and 0 for the rest of it
  */
 int _islower(int c)
