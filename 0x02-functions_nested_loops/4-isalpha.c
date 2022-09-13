545de0a184d69f24fce60215f1e67a7709a60471
solomondev01 <solomondev002@gmail.com>
2022-09-13 05:29:12 -0700

    a function that checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - Shows 1 if the input is a
+ * letter. Another cases, shows 0
+ * @c: The character in ASCII code
+ * Return: 1 for letters. 0 for the rest of it.
+ */
+int int _isalpha(int c)
+{
+	if ((c >= 97 && <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
