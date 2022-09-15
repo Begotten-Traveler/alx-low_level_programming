188449ef0e7240b23a22b0629d7f04ee7f1b5082
solomondev01 <solomondev002@gmail.com>
2022-09-15 16:13:21 -0700

    quick code error fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
- * _isupper - check for upper case letter
- * @c : character to check the case
- * Return:0 or 1
+ * _isupper - checks if uppercase
+ * @c: character to check
+ *
+ * Return: gives 1 or 0
  */
-
 int _isupper(int c)
 {
-	return (c >= 65 && c <= 90);
-
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
 }
