c1cb2a142d1a386bfcaa8b70b8f1a46d4bd08062
solomondev01 <solomondev002@gmail.com>
2022-09-19 09:54:04 -0700

    header and function fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_rev - prints a string, in reverse, followed by a new line.
@@ -17,6 +18,6 @@ void print_rev(char *s)
 	}
 
 	for (count--; count >= 0; count--)
-		_putchar(s[count]);
-	_putchar('\n');
+		putchar(s[count]);
+	putchar('\n');
 }
