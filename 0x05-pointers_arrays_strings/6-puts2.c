94acf272ec4a8b276d4ac91d9f215064631c9b41
solomondev01 <solomondev002@gmail.com>
2022-09-19 10:28:07 -0700

    oh some errors

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,20 @@
 #include "main.h"
+#include "2-strlen.c"
 /**
- * puts2 - prints a string, followed by a new line,
- * @str: pointer to the string to print
- * Return: void
-*/
-
+ * puts2 - puts string twice
+ *
+ * @str: string to put twice
+ */
 void puts2(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
-{
-	if (i % 2 == 0)
-	{
-		putchar(str[i]);
-	}
+	int i;
 
-	i++;
-}
-putchar('\n');
+	for (i = 0; i < _strlen(str); i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
 }
