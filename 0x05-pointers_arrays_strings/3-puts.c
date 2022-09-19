013867486d12693acf3d1f3db3c41d7d2da698c5
solomondev01 <solomondev002@gmail.com>
2022-09-19 09:20:22 -0700

    I am passing through a lot

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
- * _puts - prints a string, followed by a new line,
- * @str: pointer to the string to print
- * Return: void
-*/
-
-
+ * _puts - puts a string
+ *
+ * @str: string to print
+ */
 void _puts(char *str)
 {
-int i = 0;
-while (str[i])
-{
-	_putchar(str[i]);
-	i++;
-}
-_putchar('\n');
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
 }
