01fadabd4d4bf5f884abbc757330f950489f8463
solomondev01 <solomondev002@gmail.com>
2022-09-21 19:53:54 -0700

    I am trying

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
  * print_number - prints an integer.
- * @n: integer
+ * @n: integer to be printed
  * Return: void
  */
-
 void print_number(int n)
 {
 	unsigned int num = n;
